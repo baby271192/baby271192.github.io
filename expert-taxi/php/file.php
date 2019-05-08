347f1b57eda4677b5ed5a014aae0499ae10eda96
sampo6olvjopu <sampo6olvjopu@users.noreply.github.com>
2019-05-08 19:36:55 +0300

    da

--- expert-taxi/php/file.php
+++ expert-taxi/php/file.php
@@ -4,27 +4,28 @@
 $urok="Урок 22";
 error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
 // создание переменных из полей формы		
-if (isset($_POST['your-name']))			{$name1			= $_POST['your-name'];		if ($name1 == '')	{unset($name1);}}
-if (isset($_POST['email1']))		{$email1		= $_POST['email1'];		if ($email1 == '')	{unset($email1);}}
-if (isset($_POST['message']))			{$text			= $_POST['message'];		if ($text == '')	{unset($text);}}
-if (isset($_POST['submit']))			{$sab			= $_POST['submit'];		if ($sab == '')		{unset($sab);}}
+if (isset($_POST['your-name']))			{$your-name			= $_POST['your-name'];		if ($your-name == '')	{unset($your-name);}}
+
+
+if (isset($_POST['message']))			{$message			= $_POST['message'];		if ($message == '')	{unset($message);}}
+
+
+if (isset($_POST['submit']))			{$submit			= $_POST['submit'];		if ($submit == '')		{unset($submit);}}
+
+
 //стирание треугольных скобок из полей формы
 if (isset($your-name) ) {
-$name1=stripslashes($your-name);
-$name1=htmlspecialchars($your-name);
-}
-if (isset($email1) ) {
-$email1=stripslashes($email1);
-$email1=htmlspecialchars($email1);
+$your-name=stripslashes($your-name);
+$your-name=htmlspecialchars($your-name);
 }
 if (isset($message) ) {
-$text=stripslashes($message);
-$text=htmlspecialchars($message);
+$message=stripslashes($message);
+$message=htmlspecialchars($message);
 }
 // адрес почты куда придет письмо
 $address="alexandr.protik@gmail.com";
 // текст письма 
-$note_text="Тема : $urok \r\nИмя : $your-name \r\n Email : $email1 \r\n Дополнительная информация : $text";
+$note_text="Тема : $urok \r\nИмя : $your-name \r\n Email : $email1 \r\n Дополнительная информация : $message";
 
 if (isset($your-name)  &&  isset ($submit) ) {
 mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
